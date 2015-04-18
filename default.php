97f648ae6520e2e638e7a4a5274fd2e8e818648f
Steven Wilson <info@dominion79.co.uk>
2015-04-18 23:08:10 +0100

    split up header and footer

--- default.php
+++ default.php
@@ -1,39 +1,49 @@
-<!doctype html>
-<html class="no-js" lang="">
+<?php defined('C5_EXECUTE') or die("Access Denied."); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html class="no-js" lang="<?php echo Localization::activeLanguage()?>" xmlns="http://www.w3.org/1999/xhtml">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title></title>
-        <meta name="description" content="">
+       
+        <?php Loader::element('header_required')?>
+        
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
         <!-- Place favicon.ico in the root directory -->
 
-        <link rel="stylesheet" href="build/styles.css">
-        <script src="bower_components/modernizr/modernizr.js"></script>
+        <link rel="stylesheet" href="<?php echo $view->getThemePath()?>/build/styles.css">
+        <script src="<?php echo $view->getThemePath()?>/bower_components/modernizr/modernizr.js"></script>
     </head>
     <body>
-        <!--[if lt IE 8]>
-            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
+        <div class="<?php echo $c->getPageWrapperClass()?>">
+        
+          <?php $view->inc('inc/header.php'); ?>
+          
+  
+          <!-- Add your site or application content here -->
+          <p>Hello world! This is HTML5 Boilerplate.</p>
+          
+          
+          <?php $view->inc('inc/footer.php'); ?>
+          
+          <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+          <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+          <script src="<?php echo $view->getThemePath()?>/js/plugins.js"></script>
+          <script src="<?php echo $view->getThemePath()?>/js/main.js"></script>
+  
+          <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+          <script>
+              (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+              function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+              e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+              e.src='https://www.google-analytics.com/analytics.js';
+              r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+              ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+          </script>
+        
+        </div><!-- C5 wrapper -->
 
-        <!-- Add your site or application content here -->
-        <p>Hello world! This is HTML5 Boilerplate.</p>
+        <?php Loader::element('footer_required')?>
 
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
-
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='https://www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-        </script>
     </body>
 </html>
